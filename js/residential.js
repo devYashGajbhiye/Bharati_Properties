7c18e2daa8f5a1c4f60574e3aa42e5386199cbd6
Yash Priyatish Gajbhiye <your-email@example.com>
2025-12-11 17:26:37 +0530

    Completed the services page and also subpages like residential and commercial 11/12/25

---
+++ js/residential.js
@@ -0,0 +1,68 @@
+// BUY SLIDER
+const slider = document.querySelector(".slider");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+let translateX = 0;
+let cardWidth = 370;
+let maxScroll = (slider.children.length * cardWidth) - slider.parentElement.offsetWidth;
+
+nextBtn.addEventListener("click", () => {
+    if (Math.abs(translateX) < maxScroll) {
+        translateX -= cardWidth;
+        slider.style.transform = `translateX(${translateX}px)`;
+    }
+});
+
+prevBtn.addEventListener("click", () => {
+    if (translateX < 0) {
+        translateX += cardWidth;
+        slider.style.transform = `translateX(${translateX}px)`;
+    }
+});
+
+
+// RENT SLIDER
+const rentSlider = document.querySelector(".rent-slider");
+const rentNext = document.querySelector(".rent-next");
+const rentPrev = document.querySelector(".rent-prev");
+
+let rentTranslateX = 0;
+let rentMaxScroll = (rentSlider.children.length * cardWidth) - rentSlider.parentElement.offsetWidth;
+
+rentNext.addEventListener("click", () => {
+    if (Math.abs(rentTranslateX) < rentMaxScroll) {
+        rentTranslateX -= cardWidth;
+        rentSlider.style.transform = `translateX(${rentTranslateX}px)`;
+    }
+});
+
+rentPrev.addEventListener("click", () => {
+    if (rentTranslateX < 0) {
+        rentTranslateX += cardWidth;
+        rentSlider.style.transform = `translateX(${rentTranslateX}px)`;
+    }
+});
+
+
+// SELL SLIDER
+const sellSlider = document.querySelector(".sell-slider");
+const sellNext = document.querySelector(".sell-next");
+const sellPrev = document.querySelector(".sell-prev");
+
+let sellTranslateX = 0;
+let sellMaxScroll = (sellSlider.children.length * cardWidth) - sellSlider.parentElement.offsetWidth;
+
+sellNext.addEventListener("click", () => {
+    if (Math.abs(sellTranslateX) < sellMaxScroll) {
+        sellTranslateX -= cardWidth;
+        sellSlider.style.transform = `translateX(${sellTranslateX}px)`;
+    }
+});
+
+sellPrev.addEventListener("click", () => {
+    if (sellTranslateX < 0) {
+        sellTranslateX += cardWidth;
+        sellSlider.style.transform = `translateX(${sellTranslateX}px)`;
+    }
+});
